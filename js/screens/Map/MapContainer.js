b0817502701df2fa101c8243b8279f8c7fbcf166
Eric Chan <ericchan277@gmail.com>
2018-06-04 02:54:55 -0700

    Removes api key from map

--- js/screens/Map/MapContainer.js
+++ js/screens/Map/MapContainer.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import Map from './Map';
 
-const mapKey = 'AIzaSyChBkZ0lqkmwPNCp93Jh0dr_bD5xPzzNPI';
-
 class MapContainer extends Component {
   render() {
     return (
